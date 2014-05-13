56b8548b77f7f1fd4d930ceaf0cc54957733e578
Michael <mnichols612@mail.northark.edu>
2014-05-13 08:08:12 -0500

    Connecting
    Got it to connect. Not pinging or accepting input from irc yet, though
    the TellAJoke method was working on console prior to starting to move it
    onto IRC. Also got methods in place to allow UI to interact with
    StreamReader and StreamWriter.

---
+++ BusinessLayer/BusinessLayer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntitiesLayer;
+using System.IO;
+using DataAccess;
+
+namespace BusinessLayer
+{
+    public static class BLL
+    {
+        public static IRC irc = new IRC();
+        public static WebPage page = new WebPage();
+        public static DataAccess.File file = new DataAccess.File();
+
+        #region File Methods
+        public static string TellAJoke()
+        {
+            string joke;
+
+            joke = file.GetJoke();
+
+            return joke;
+        }
+        #endregion
+
+        #region IRC Methods
+        public static StreamReader GetIrcReader()
+        {
+            return irc.Reader;
+        }
+
+        public static StreamWriter GetIrcWriter()
+        {
+            return irc.Writer;
+        }
+
+        public static void IrcDisconnect()
+        {
+            irc.Disconnect();
+        }
+
+        public static void Connect()
+        {
+            irc.Connect("chat.freenode.net,6667");
+        }
+
+        public static string Locate(string nick)
+        {
+                string location;
+                location = irc.Locate(nick);
+                return location;
+        }
+        #endregion
+    }
+}
---
+++ JunkBot/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using BusinessLayer;
+using EntitiesLayer;
+
+namespace JunkBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BLL.Connect();
+
+                while (true)
+                {
+                    string input;
+
+                    while ((input = BLL.GetIrcReader().ReadLine()) != null)
+                    {
+                        if (input.ToLower().StartsWith("locate "))
+                        {
+                            string nick = input.Substring(input.IndexOf(" "));
+
+                            try
+                            {
+                                Console.WriteLine(nick + " is located at " + BLL.Locate(nick));
+                            }
+                            catch
+                            {
+                                Console.WriteLine(nick + " could not be located.");
+                            }
+                        }
+                        else if (input.ToLower().Contains("joke"))
+                        {
+                            Console.WriteLine(BLL.TellAJoke());
+                        }
+                    }
+                }
+
+                //I don't think it will pose a problem as it accesses it through a method in BLL, but there is a warning here about "unreachable code"
+                BLL.IrcDisconnect();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
