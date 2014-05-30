0b99e64cf5498cf1a46f5d6e887392205f56fb0f
Michael <mnichols612@mail.northark.edu>
2014-05-30 16:17:46 -0500

    Plugins, 2 layers
    Removed Business and DataAccess layers in favor of using plugins.

---
+++ BusinessLayer/Plugins.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Plugins
+{
+    public interface IPlugin
+    {
+        string Name { get; }
+
+        string OnMessage();
+    }
+
+    public class Jokes : IPlugin
+    {
+        public static FileStream stream;
+        public static StreamReader reader;
+
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string OnMessage()
+        {
+
+            string joke = "";
+
+            int line = 0, lines = 0;
+            Random r = new Random();
+
+            stream = new FileStream("C:\\Users\\Michael\\Documents\\Visual Studio 2012\\Projects\\JunkBot\\Documents\\Jokes.txt", FileMode.Open);
+
+            reader = new StreamReader(stream);
+
+            while (reader.ReadLine() != null)
+            {
+                lines++;
+            }
+
+            line = r.Next(0, lines);
+            lines = 0;
+            stream.Position = 0;
+            reader.DiscardBufferedData();
+
+            while (reader.ReadLine() != null)
+            {
+                lines++;
+                if (lines == line)
+                {
+                    joke = reader.ReadLine();
+                    break;
+                }
+            }
+
+            stream.Close();
+
+            return joke;
+        }
+    }
+}
--- JunkBot/Program.cs
+++ JunkBot/Program.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
 using System.IO;
-using BusinessLayer;
+using Plugins;
 
 namespace JunkBot
 {
@@ -14,21 +15,43 @@ namespace JunkBot
         {
             try
             {
-                BLL.Connect();
+                TcpClient irc;
+                NetworkStream stream;
+                StreamReader reader;
+                StreamWriter writer;
+
+                irc = new TcpClient("chat.freenode.net", 6667);
+                stream = irc.GetStream();
+                reader = new StreamReader(stream);
+                writer = new StreamWriter(stream);
+                writer.WriteLine("USER JunkBot 8 * :C#Bot");
+                writer.Flush();
+                writer.WriteLine("NICK JunkBot");
+                writer.Flush();
+                writer.WriteLine("JOIN ##DiCrew");
+                writer.Flush();
 
                 while (true)
                 {
                     string input;
+                    IPlugin plugin;
 
-                    while ((input = BLL.GetIrcReader().ReadLine()) != null)
+                    while ((input = reader.ReadLine()) != null)
                     {
+                        string[] ex = input.Split(' ');
+
+                        if (ex[0] == "PING")
+                        {
+                            writer.WriteLine("PONG"+" "+ex[1]);
+                        }
+
                         if (input.ToLower().StartsWith("locate "))
                         {
                             string nick = input.Substring(input.IndexOf(" "));
 
                             try
                             {
-                                Console.WriteLine(nick + " is located at " + BLL.Locate(nick));
+                                //Look into how to locate a user's city and state by IP address or a simpler way if there is one.
                             }
                             catch
                             {
@@ -37,13 +60,17 @@ namespace JunkBot
                         }
                         else if (input.ToLower().Contains("joke"))
                         {
-                            Console.WriteLine(BLL.TellAJoke());
+                            plugin = new Jokes();
+                            writer.WriteLine(plugin.OnMessage());
                         }
                     }
                 }
 
-                //I don't think it will pose a problem as it accesses it through a method in BLL, but there is a warning here about "unreachable code"
-                BLL.IrcDisconnect();
+                //writer.Close() says "unsearchable code" is detected.
+                //need to look into what code it is referring to.
+                writer.Close();
+                reader.Close();
+                irc.Close();
             }
             catch
             {
